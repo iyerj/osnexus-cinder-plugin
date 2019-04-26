ffb88e8502b6aed87f34f110606cfb2669b80830
Yachika Ralhan <yachika.ralhan@msystechnologies.com>
2019-04-26 17:08:45 +0530

    quantastor code from git

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -19,15 +19,19 @@ if [[ "$1" == "stack" && "$2" == "install" ]]; then
     if [ -z "$LATEST_PATCHSET" ]; then
         echo "Failed to determine latest patchset of $PATCHSET_BASE from $UPSTREAM_REMOTE"
     fi
-
+     
     echo "Latest patchset ref is $LATEST_PATCHSET"
     touch /home/tempest/devstack/commit-id
     echo 'cinder_commit_id ' | tr -d '\n'  > /home/tempest/devstack/commit-id && git rev-parse --short HEAD >> /home/tempest/devstack/commit-id
 
+    git clone https://gitlab.com/osnexus/sos-ci.git /opt/stack/quantastor
     if [ ! -e /opt/stack/cinder/cinder/volume/drivers/quantastor.py ]; then
         #git fetch $UPSTREAM_REMOTE $LATEST_PATCHSET && git cherry-pick FETCH_HEAD
-        cp /opt/stack/quantastor/devstack/quantastor.py /opt/stack/cinder/cinder/volume/drivers/quantastor.py
-        cp /opt/stack/quantastor/devstack/quantastor_api.py /opt/stack/cinder/cinder/volume/drivers/quantastor_api.py
+        #cp /opt/stack/quantastor/devstack/quantastor.py /opt/stack/cinder/cinder/volume/drivers/quantastor.py
+        #cp /opt/stack/quantastor/devstack/quantastor_api.py /opt/stack/cinder/cinder/volume/drivers/quantastor_api.py
+
+	cp /opt/stack/quantastor/sos-ci/cinder-qstor-plugin/quantastor.py /opt/stack/cinder/cinder/volume/drivers/quantastor.py
+	cp /opt/stack/quantastor/sos-ci/cinder-qstor-plugin/quantastor_api.py /opt/stack/cinder/cinder/volume/drivers/quantastor_api.py
     fi
 
 
